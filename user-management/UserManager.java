e2b9cc0e248ba97994dc4aa2521ad063a29d40a7
Gwyn-Dayot <20230028015@my.xu.edu.ph>
2025-05-25 19:21:51 +0800

    userManager-up

---
+++ user-management/UserManager.java
@@ -0,0 +1,69 @@
+
+import java.lang.classfile.attribute.StackMapTableAttribute;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserManager {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/FlashCard_db";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "xxxx";
+
+    private Connection connection;
+
+    public UserManager() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to connect to the database");
+        }
+    }
+
+    public boolean addUser(String username, String password) {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        try (var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            int rowsInserted = stmt.executeUpdate();
+            return rowsInserted > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean validateUser(String username, String password) {
+        if (username == null || password == null) return false;
+
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, username);
+        
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String dbPassword = rs.getString("password");
+                return dbPassword.equals(password);
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void close() {
+        try {
+            if (connection != null && !connection.isClosed())
+                connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
